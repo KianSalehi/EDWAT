39e0ba622e9dd0f51ad56a7f7961cc22945ba10c
Kian Salehi <kian.k.salehi@gmail.com>
2021-08-05 03:08:00 -0400

    Video.js components added, chat added, iframe for youtube added, link query added

--- src/components/screens/Video.js
+++ src/components/screens/Video.js
@@ -1,12 +1,105 @@
-import React, { useState } from "react";
-import { Container } from "@material-ui/core";
-import Chat from './chat/Chat'
-function Video() {
+import React, { useState, useRef } from "react";
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {ChatContainer, MessageList, Message, MessageInput, MainContainer} from "@chatscope/chat-ui-kit-react"
+import chatBackgroundVideo from './background/chatBackground.mp4';
+import { Grid, TextField, Button} from "@material-ui/core";
+import {makeStyles} from '@material-ui/core/styles';
+
+function Video(props) {
+  const inputRef = useRef();
+  const [inputMessage, setInputMessage]=useState("");
+  const [messages, setMessages]=useState([]);
+  const handleSend=(message)=>{
+    setMessages([...messages, {
+      message,
+      direction:"outgoing",
+      sender:"Me"
+    }]);
+    setInputMessage("");
+    inputRef.current.focus();
+
+  }
+  const embedID="xOQ2QaUuQ8U"
+  const classes = useStyles();
+
   return (
-    <div style={{position: "absolute", right:"0", height: "100%"}}>
-      <Chat></Chat>
+    <Grid container direction="row">
+    <Grid item>
+      <Grid container direction="column">
+        <Grid item className={classes.videoBox}>
+          <iframe
+          width="100%"
+          height="100%"
+          src={'https:www.youtube.com/embed/'+embedID}
+          allowFullScreen
+          allow="accelerometer; autoplay; clipboard-write; encrypter-mdia; gyroscope; picture-in-pricture">
+          </iframe>
+        </Grid>
+        <Grid item className={classes.searchBar}>
+          <Grid container direction="row" >
+            <Grid item>
+              <TextField variant="filled" className={classes.queryBox}></TextField>
+            </Grid>
+            <Grid item>
+              <Button variant="contained" label="please put the link here" placeholder="Please put the link here" color="primary" className={classes.queryButton}>Search</Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+    <Grid item>
+    <div style={{
+      position:"fixed",
+      right:"0",
+      height:"100%",
+      width:"20%",
+    }}>
+      <MainContainer>
+      <ChatContainer>
+        <MessageList>
+          <Message
+          model={{
+            message: "Your room ID is: "+props.room,
+            sentTime: "just now",
+            direction:"incoming",
+            sender: "Kian",
+          }}
+          />
+          {messages.map((m,i)=> <Message key={i} model={m}/>)}
+        </MessageList>
+        <MessageInput placeholder="Type message here" onSend={handleSend} onChange={setInputMessage} value={inputMessage} ref={inputRef}/>
+      </ChatContainer>
+      </MainContainer>
     </div>
+    </Grid>
+    </Grid>
   )
 }
+
+
+const useStyles = makeStyles({
+  searchBar:{
+    position:"fixed",
+    bottom:0,
+    left:0,
+  },
+  queryBox:{
+    width:"75vw",
+    height:"3vw"
+  },
+  queryButton:{
+    height:"3vw",
+    width:"5vw"
+  },
+  videoBox:{
+    position:"fixed",
+    top:0,
+    left:0,
+    width:"80%",
+    height:"94%"
+  }
+});
+
+
 
 export default Video;
--- src/components/screens/chat/Chat.js
+++ src/components/screens/chat/Chat.js
@@ -1,30 +1,30 @@
 import React from "react";
-import { Grid, IconButton, TextField } from "@material-ui/core";
+import { Grid, IconButton, TextField, Paper } from "@material-ui/core";
 import SendIcon from '@material-ui/icons/Send'
 function Chat() {
   return (
-    <div>
-    <Grid container spacing={1} direction="column" alignItems="center">
-    <Grid item>
-      <div style={{height: '90%'}}>{/* To hold the chat messages */}</div>
-    </Grid>
-    <Grid item>
-      <div style={{height:'10%'}}>
-        {/* To hold the text field and send button*/}
-        <Grid container spacing={1} direction = "row" alignItems="center" justifyContent="center">
-          <Grid item>
-              <TextField placeholder='message...'></TextField>
-          </Grid>
-          <Grid item>
-            <IconButton color="primary" aria-label="send button" size="medium">
-              <SendIcon />
-            </IconButton>
-          </Grid>
+      <Grid container spacing={5} direction="column">
+        <div style={{width:"18%",height:"90%", right:0, top:0, position:"absolute", backgroundColor:"black"}}>
+        <Grid item>
+          <Paper></Paper>
         </Grid>
-      </div>
-    </Grid>
-    </Grid>
-    </div>
+        </div>
+        <Grid item>
+          <div style={{width:"18%", position:"absolute", right:0, bottom:0}}>
+            {/* To hold the text field and send button*/}
+            <Grid container spacing={1} direction="row" alignItems="center" justifyContent="center">
+              <Grid item>
+                <TextField placeholder='message...'></TextField>
+              </Grid>
+              <Grid item>
+                <IconButton color="primary" aria-label="send button" size="medium">
+                  <SendIcon />
+                </IconButton>
+              </Grid>
+            </Grid>
+          </div>
+        </Grid>
+      </Grid>
   );
 }
 
