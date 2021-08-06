a5a70e9493cc0b6011ab31095fbc5aa9455609b6
Kian Salehi <kian.k.salehi@gmail.com>
2021-08-06 18:24:07 -0400

    video.js styling changed

--- src/components/screens/Video.js
+++ src/components/screens/Video.js
@@ -1,9 +1,9 @@
 import React, { useState, useRef } from "react";
-import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import styles from '../../themes/main.scss';
 import {ChatContainer, MessageList, Message, MessageInput, MainContainer} from "@chatscope/chat-ui-kit-react"
-import chatBackgroundVideo from './background/chatBackground.mp4';
-import { Grid, TextField, Button} from "@material-ui/core";
+import { Grid, Paper, IconButton, InputBase} from "@material-ui/core";
 import {makeStyles} from '@material-ui/core/styles';
+import {Search} from '@material-ui/icons';
 
 function Video(props) {
   const inputRef = useRef();
@@ -23,7 +23,8 @@ function Video(props) {
   const classes = useStyles();
 
   return (
-    <Grid container direction="row">
+
+    <Grid container direction="row" className={classes.mainBackground} >
     <Grid item>
       <Grid container direction="column">
         <Grid item className={classes.videoBox}>
@@ -36,14 +37,16 @@ function Video(props) {
           </iframe>
         </Grid>
         <Grid item className={classes.searchBar}>
-          <Grid container direction="row" >
-            <Grid item>
-              <TextField variant="filled" className={classes.queryBox}></TextField>
-            </Grid>
-            <Grid item>
-              <Button variant="contained" label="please put the link here" placeholder="Please put the link here" color="primary" className={classes.queryButton}>Search</Button>
-            </Grid>
-          </Grid>
+          <Paper component="form" className={classes.root}>
+            <InputBase
+                className={classes.input}
+                placeholder="Search Youtube"
+                inputProps={{ 'aria-label': 'search YouTube' }}
+            />
+            <IconButton type="submit" className={classes.iconButton} aria-label="search">
+              <Search color="action"/>
+            </IconButton>
+          </Paper>
         </Grid>
       </Grid>
     </Grid>
@@ -53,6 +56,7 @@ function Video(props) {
       right:"0",
       height:"100%",
       width:"20%",
+      backgroundColor:"#3d3d3d"
     }}>
       <MainContainer>
       <ChatContainer>
@@ -65,7 +69,7 @@ function Video(props) {
             sender: "Kian",
           }}
           />
-          {messages.map((m,i)=> <Message key={i} model={m}/>)}
+          {messages.map((m,i)=> <Message key={i} model={m}><Message.Header sender="me" sentTime="now"/></Message>)}
         </MessageList>
         <MessageInput placeholder="Type message here" onSend={handleSend} onChange={setInputMessage} value={inputMessage} ref={inputRef}/>
       </ChatContainer>
@@ -79,25 +83,43 @@ function Video(props) {
 
 const useStyles = makeStyles({
   searchBar:{
-    position:"fixed",
+    position:"absolute",
     bottom:0,
     left:0,
-  },
-  queryBox:{
-    width:"75vw",
-    height:"3vw"
-  },
-  queryButton:{
-    height:"3vw",
-    width:"5vw"
+    backgroundColor:"#3d3d3d",
+
   },
   videoBox:{
-    position:"fixed",
+    position:"absolute",
     top:0,
     left:0,
     width:"80%",
-    height:"94%"
-  }
+    height:"95%",
+    backgroundColor:"#3d3d3d",
+  },
+  mainBackground:{
+    backgroundColor:"#3d3d3d",
+  },
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: "79.5vw",
+    backgroundColor:"#3d3d3d",
+    border:"1px",
+    borderColor:"white",
+    borderStyle:"solid"
+  },
+  input: {
+    flex: 1,
+    color:"white",
+
+  },
+  iconButton: {
+    padding: 10,
+    backgroundColor:"#6FA9FF"
+  },
+
 });
 
 
